3277506d706c2be587e7dbf15343b0825d808b63
leo-guinan <leo@guinantech.com>
2021-08-16 17:28:08 -0400

    adding link to sign up button

--- app/core/components/CTA.tsx
+++ app/core/components/CTA.tsx
@@ -1,3 +1,5 @@
+import { Link, Routes } from "blitz"
+
 const CTA = () => {
   return (
     <div className="bg-white dark:bg-gray-800 ">
@@ -8,12 +10,14 @@ const CTA = () => {
         </h2>
         <div className="lg:mt-0 lg:flex-shrink-0">
           <div className="mt-12 inline-flex rounded-md shadow">
-            <button
-              type="button"
-              className="py-4 px-6  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
-            >
-              Sign up for free.
-            </button>
+            <Link href={Routes.SignupPage()}>
+              <button
+                type="button"
+                className="py-4 px-6  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+              >
+                Sign up for free.
+              </button>
+            </Link>
           </div>
         </div>
       </div>
