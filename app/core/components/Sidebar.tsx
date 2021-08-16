fdc52b0005db114b52b6017e8802bb8c740c73f4
leo-guinan <leo@guinantech.com>
2021-08-16 17:07:04 -0400

    cleaning up non logged in sidebar

--- app/core/components/Sidebar.tsx
+++ app/core/components/Sidebar.tsx
@@ -17,21 +17,35 @@ const Sidebar = () => {
             {!currentUser && (
               <Fragment>
                 <Link href={Routes.SignupPage()}>
-                  <a className="button small">
-                    <strong>Sign Up</strong>
+                  <a
+                    className="hover:text-gray-800 hover:bg-gray-100 flex items-center p-2 my-6 transition-colors dark:hover:text-white dark:hover:bg-gray-600 duration-200  text-gray-600 dark:text-gray-400 rounded-lg "
+                    href="#"
+                  >
+                    <span className="mx-4 text-lg font-normal">Sign Up</span>
+                    <span className="flex-grow text-right"></span>
                   </a>
                 </Link>
                 <div></div>
                 <Link href={Routes.LoginPage()}>
-                  <a className="button small">
-                    <strong>Login</strong>
+                  <a
+                    className="hover:text-gray-800 hover:bg-gray-100 flex items-center p-2 my-6 transition-colors dark:hover:text-white dark:hover:bg-gray-600 duration-200  text-gray-600 dark:text-gray-400 rounded-lg "
+                    href="#"
+                  >
+                    <span className="mx-4 text-lg font-normal">Login</span>
+                    <span className="flex-grow text-right"></span>
                   </a>
                 </Link>
               </Fragment>
             )}
-            {!currentUser?.twitterUsername && (
+            {currentUser && !currentUser.twitterUsername && (
               <Fragment>
-                <a href="/api/auth/twitter">Log In With Twitter</a>
+                <a
+                  className="hover:text-gray-800 hover:bg-gray-100 flex items-center p-2 my-6 transition-colors dark:hover:text-white dark:hover:bg-gray-600 duration-200  text-gray-600 dark:text-gray-400 rounded-lg "
+                  href="/api/auth/twitter"
+                >
+                  <span className="mx-4 text-lg font-normal">Log In With Twitter</span>
+                  <span className="flex-grow text-right"></span>
+                </a>
               </Fragment>
             )}
             {currentUser?.twitterUsername && (
