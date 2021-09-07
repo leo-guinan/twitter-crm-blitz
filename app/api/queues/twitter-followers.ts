8bfd538381bc61ece5955776d6c67d1ed9b165f6
leo-guinan <leo@guinantech.com>
2021-09-07 14:58:06 -0400

    forgot to fix .gitignore

---
+++ app/api/queues/twitter-followers.ts
@@ -0,0 +1,44 @@
+//inital load of twitter account data for new user
+import { Queue } from "quirrel/next"
+import db, { RelationshipType, ProcessingStatus } from "db"
+import Twitter from "twitter-lite"
+import twitterFollowers from "./twitter-followers"
+import { add } from "date-fns"
+
+export default Queue(
+  "api/queues/twitter-followers", // 👈 the route it's reachable on
+  async (job: { userId }) => {
+    try {
+      const user = await db.user.findFirst({
+        where: { id: job.userId },
+        select: {
+          id: true,
+          memberships: {
+            select: {
+              organization: {
+                select: {
+                  twitterAccounts: {
+                    select: {
+                      id: true,
+                      twitterId: true,
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      })
+      if (user?.memberships[0]?.organization?.twitterAccounts[0]?.twitterId) {
+        await db.twitterDataPull.create({
+          data: {
+            twitterAccountId: user?.memberships[0]?.organization?.twitterAccounts[0]?.id,
+            relationshipType: RelationshipType.FOLLOWER,
+          },
+        })
+      }
+    } catch (e) {
+      console.log("exception processing: " + e)
+    }
+  }
+)
---
+++ app/api/queues/twitter-following.ts
@@ -0,0 +1,45 @@
+//inital load of twitter account data for new user
+import { Queue } from "quirrel/next"
+import db, { RelationshipType, ProcessingStatus } from "db"
+import Twitter from "twitter-lite"
+import twitterFollowing from "./twitter-following"
+import { add } from "date-fns"
+
+export default Queue(
+  "api/queues/twitter-following", // 👈 the route it's reachable on
+  async (job: { userId }) => {
+    try {
+      const user = await db.user.findFirst({
+        where: { id: job.userId },
+        select: {
+          id: true,
+          twitterId: true,
+          memberships: {
+            select: {
+              organization: {
+                select: {
+                  twitterAccounts: {
+                    select: {
+                      id: true,
+                      twitterId: true,
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      })
+      if (user?.memberships[0]?.organization?.twitterAccounts[0]?.twitterId) {
+        await db.twitterDataPull.create({
+          data: {
+            twitterAccountId: user?.memberships[0]?.organization?.twitterAccounts[0]?.id,
+            relationshipType: RelationshipType.FOLLOWING,
+          },
+        })
+      }
+    } catch (e) {
+      console.log("exception processing: " + e)
+    }
+  }
+)
