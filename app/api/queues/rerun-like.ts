b9ebca186d555ceac629411690d470de20d7df71
leo-guinan <leo@copy.ai>
2022-01-29 17:28:34 -0500

    adding bearer token for non-authenticated users

--- app/api/queues/rerun-like.ts
+++ app/api/queues/rerun-like.ts
@@ -12,15 +12,28 @@ export default Queue("api/queues/rerun-like", async ({ twitterId, tweetId }) => 
   if (!authenticatedUser) {
     throw new Error("No authenticated user found")
   }
-  const client = new Twitter({
-    subdomain: "api", // "api" is the default (change for other subdomains)
-    version: "2", // version "1.1" is the default (change for other subdomains)
-    extension: false,
-    consumer_key: process.env.TWITTER_CONSUMER_KEY as string, // from Twitter.
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET as string, // from Twitter.
-    access_token_key: authenticatedUser.twitterToken as string, // from your User (oauth_token)
-    access_token_secret: authenticatedUser.twitterSecretToken as string, // from your User (oauth_token_secret)
-  })
+  let client
+
+  if (!authenticatedUser.twitterToken) {
+    client = new Twitter({
+      subdomain: "api", // "api" is the default (change for other subdomains)
+      version: "2", // version "1.1" is the default (change for other subdomains)
+      extension: false,
+      consumer_key: process.env.TWITTER_CONSUMER_KEY as string, // from Twitter.
+      consumer_secret: process.env.TWITTER_CONSUMER_SECRET as string, // from Twitter.
+      bearer_token: process.env.TWITTER_BEARER_TOKEN as string,
+    })
+  } else {
+    client = new Twitter({
+      subdomain: "api", // "api" is the default (change for other subdomains)
+      version: "2", // version "1.1" is the default (change for other subdomains)
+      extension: false,
+      consumer_key: process.env.TWITTER_CONSUMER_KEY as string, // from Twitter.
+      consumer_secret: process.env.TWITTER_CONSUMER_SECRET as string, // from Twitter.
+      access_token_key: authenticatedUser.twitterToken as string, // from your User (oauth_token)
+      access_token_secret: authenticatedUser.twitterSecretToken as string, // from your User (oauth_token_secret)
+    })
+  }
 
   await getLikes(client, twitterId, tweetId)
 })
