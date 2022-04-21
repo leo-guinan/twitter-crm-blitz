d4b988d7dd6fc1ae86862b566be18b5f437d4c59
leo-guinan <leo@copy.ai>
2022-04-21 13:18:12 -0400

    fixing issue with update queue

--- app/api/queues/process-twitter-account.ts
+++ app/api/queues/process-twitter-account.ts
@@ -2,11 +2,17 @@ import { Queue } from "quirrel/next"
 import { getEngagement } from "../../util/twitter/engagement"
 import { getClientForAccount } from "../../util/twitter/client"
 import { refreshUser } from "../../util/twitter/populate-user"
-
+import db from "db"
 export default Queue(
   "api/queues/process-twitter-account",
   async ({ twitterAccountTwitterId }) => {
-    const client = await getClientForAccount(twitterAccountTwitterId)
+    const twitterAccount = await db.twitterAccount.findFirst({
+      where: {
+        twitterId: twitterAccountTwitterId,
+      },
+    })
+
+    const client = await getClientForAccount(twitterAccount)
     await refreshUser(client, twitterAccountTwitterId)
     await getEngagement(client, twitterAccountTwitterId)
   },
